381189f9506d12e8a79efcf318ac59e782cca0bb
Darkmind223 <86848731+Darkmind223@users.noreply.github.com>
2025-09-08 00:06:00 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const typedTextElement = document.getElementById('animated-name'); // Changed ID
+    const phrases = [
+        "Sira Diarra",
+        "Étudiante en Informatique",
+        "Développeuse Junior",
+        "Passionnée de technologie",
+        "et d'innovation",
+        "Toujours motivée à apprendre"
+       
+    ];
+
+    let phraseIndex = 0;
+    let charIndex = 0;
+    let isDeleting = false;
+
+    function typeWriter() {
+        const currentPhrase = phrases[phraseIndex];
+
+        if (isDeleting) {
+            typedTextElement.textContent = currentPhrase.substring(0, charIndex - 1);
+            charIndex--;
+        } else {
+            typedTextElement.textContent = currentPhrase.substring(0, charIndex + 1);
+            charIndex++;
+        }
+
+        let typeSpeed = 100; // Vitesse de frappe
+        if (isDeleting) typeSpeed /= 2; // Vitesse d'effacement plus rapide
+
+        if (!isDeleting && charIndex === currentPhrase.length) {
+            typeSpeed = 1500; // Pause à la fin de la frappe
+            isDeleting = true;
+        } else if (isDeleting && charIndex === 0) {
+            isDeleting = false;
+            phraseIndex = (phraseIndex + 1) % phrases.length;
+            typeSpeed = 500; // Pause avant de taper la phrase suivante
+        }
+
+        setTimeout(typeWriter, typeSpeed);
+    }
+
+    typeWriter();
+});
